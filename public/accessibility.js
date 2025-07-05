ff6d9ed7500dd069ed79517bbbcb83ad708f0e99
rgarg-lab <rgarg161206@gmail.com>
2025-07-05 17:37:31 +0530

    Update accessibility.js

--- public/accessibility.js
+++ public/accessibility.js
@@ -1,4 +1,3 @@
-
 function toggleColorblindMode() {
   document.body.classList.toggle("colorblind");
 }
@@ -7,6 +6,11 @@ function speakPage() {
   const topic = document.getElementById("topicInput").value || "no topic entered";
   const msg = `This is a world news map. Enter a topic like Ukraine War. Then click any country on the map to fetch news. Current topic is ${topic}.`;
   const utterance = new SpeechSynthesisUtterance(msg);
+  utterance.lang = "en-US";
+  speechSynthesis.speak(utterance);
+}
+function speakText(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
   utterance.lang = "en-US";
   speechSynthesis.speak(utterance);
 }
